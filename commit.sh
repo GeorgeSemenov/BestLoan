fa8a882a4f5a7aed61e02e9a9c72c690cc179993
GeorgeSemenov <zhora.sem@yandex.ru>
2018-11-14 16:26:31 +0300

    Теперь закончил .. наверное

--- commit.sh
+++ commit.sh
@@ -2,6 +2,8 @@
 git add -A . && \
 echo "---------------------------------Введи текст комита, будь человеком" && \
 read commitMessage && \
-git push origin
+
+git push origin && \
+
 git commit -m "$commitMessage" && \
 
